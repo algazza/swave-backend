40a74065cc4b6cf1bc677677ec752d5c3420c4e5
algazza <fadhilaswadira72564@gmail.com>
2025-11-27 08:24:59 +0700

    ✨feat: add auth controller

---
+++ prisma/migrations/20251125083253_db_swave/migration.sql
@@ -0,0 +1,172 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `role` ENUM('user', 'admin') NOT NULL DEFAULT 'user',
+    `name` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `phone` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `users_username_key`(`username`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `address` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `recipient` VARCHAR(191) NOT NULL,
+    `label` ENUM('rumah', 'kantor', 'kos', 'sekolah', 'apartemen') NOT NULL,
+    `city` VARCHAR(191) NOT NULL,
+    `subdistrict` VARCHAR(191) NOT NULL,
+    `zip_code` INTEGER NOT NULL,
+    `address` VARCHAR(191) NOT NULL,
+    `main_address` BOOLEAN NOT NULL,
+    `user_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `products` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `sold` INTEGER NOT NULL,
+    `categories_id` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `product_images` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `image_path` VARCHAR(191) NOT NULL,
+    `produt_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `categories` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `category` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `variants` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `variant` VARCHAR(191) NOT NULL,
+    `price` INTEGER NOT NULL,
+    `Stock` INTEGER NOT NULL,
+    `categories_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `carts` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `quantity` INTEGER NOT NULL,
+    `price` INTEGER NOT NULL,
+    `product_id` INTEGER NOT NULL,
+    `variant_id` INTEGER NOT NULL,
+    `user_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `product_checkouts` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `quantity` INTEGER NOT NULL,
+    `price` INTEGER NOT NULL,
+    `product_id` INTEGER NOT NULL,
+    `variant_id` INTEGER NOT NULL,
+    `checkout_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `deliveries` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `delivery_type` ENUM('delivery', 'pickup') NOT NULL,
+    `pickup_date` VARCHAR(191) NULL,
+    `pickup_hour` VARCHAR(191) NULL,
+    `delivery_price` INTEGER NOT NULL,
+    `address_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `checkouts` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `order_id` INTEGER NOT NULL,
+    `total_price` INTEGER NOT NULL,
+    `estimation` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NULL,
+    `gift_card` BOOLEAN NOT NULL,
+    `gift_description` VARCHAR(191) NULL,
+    `snap_token` VARCHAR(191) NULL,
+    `user_id` INTEGER NOT NULL,
+    `delevery_id` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `checkouts_order_id_key`(`order_id`),
+    UNIQUE INDEX `checkouts_delevery_id_key`(`delevery_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `status` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `status_type` ENUM('pending', 'delivery', 'cancel', 'success') NOT NULL,
+    `description` VARCHAR(191) NULL,
+    `checkout_id` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `address` ADD CONSTRAINT `address_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `products` ADD CONSTRAINT `products_categories_id_fkey` FOREIGN KEY (`categories_id`) REFERENCES `categories`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `product_images` ADD CONSTRAINT `product_images_produt_id_fkey` FOREIGN KEY (`produt_id`) REFERENCES `products`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `variants` ADD CONSTRAINT `variants_categories_id_fkey` FOREIGN KEY (`categories_id`) REFERENCES `categories`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `carts` ADD CONSTRAINT `carts_product_id_fkey` FOREIGN KEY (`product_id`) REFERENCES `products`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `carts` ADD CONSTRAINT `carts_variant_id_fkey` FOREIGN KEY (`variant_id`) REFERENCES `variants`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `carts` ADD CONSTRAINT `carts_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `product_checkouts` ADD CONSTRAINT `product_checkouts_product_id_fkey` FOREIGN KEY (`product_id`) REFERENCES `products`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `product_checkouts` ADD CONSTRAINT `product_checkouts_variant_id_fkey` FOREIGN KEY (`variant_id`) REFERENCES `variants`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `deliveries` ADD CONSTRAINT `deliveries_address_id_fkey` FOREIGN KEY (`address_id`) REFERENCES `address`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `checkouts` ADD CONSTRAINT `checkouts_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `checkouts` ADD CONSTRAINT `checkouts_delevery_id_fkey` FOREIGN KEY (`delevery_id`) REFERENCES `deliveries`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `status` ADD CONSTRAINT `status_checkout_id_fkey` FOREIGN KEY (`checkout_id`) REFERENCES `checkouts`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import { Hono } from 'hono'
+import { authRoute } from './routes/auth.route'
 
-const app = new Hono()
+const app = new Hono().basePath('/api')
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.route('/auth', authRoute)
 
 export default app
