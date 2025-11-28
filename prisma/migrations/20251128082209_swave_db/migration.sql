2af02e2b0bbf79beace94d86952bc120e69bc8c5
algazza <fadhilaswadira72564@gmail.com>
2025-11-28 20:31:27 +0700

    📝docs: update migration

--- prisma/migrations/20251128082209_swave_db/migration.sql
+++ prisma/migrations/20251128082209_swave_db/migration.sql
@@ -52,6 +52,7 @@ CREATE TABLE `categories` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `category` VARCHAR(191) NOT NULL,
 
+    UNIQUE INDEX `categories_category_key`(`category`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
