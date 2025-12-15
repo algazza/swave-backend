c4b58811a393499dd743e9c4b1094eb94d3d7a7f
algazza <fadhilaswadira72564@gmail.com>
2025-12-15 17:35:17 +0700

    ♻️refactor: add address description

--- prisma/migrations/20251215103428_cb_swave/migration.sql
+++ prisma/migrations/20251215103428_cb_swave/migration.sql
@@ -24,6 +24,7 @@ CREATE TABLE `address` (
     `user_id` INTEGER NOT NULL,
     `longitude` VARCHAR(191) NULL,
     `latitude` VARCHAR(191) NULL,
+    `description` VARCHAR(191) NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
