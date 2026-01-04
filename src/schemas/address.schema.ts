aefbe95c7a45e82524083f34f5668a7cead2cabd
algazza <fadhilaswadira72564@gmail.com>
2026-01-04 17:09:46 +0700

    ⚡️refactor: change enum address

--- src/schemas/address.schema.ts
+++ src/schemas/address.schema.ts
@@ -2,7 +2,7 @@ import z from "zod";
 
 export const addressSchema = z.object({
   recipient: z.string(),
-  label: z.enum(["rumah", "kantor", "kos", "sekolah", "apartemen"]),
+  label: z.enum(["Home", "Office", "Boarding House", "School", "Apartment"]),
   city: z.string(),
   subdistrict: z.string(),
   zip_code: z.string().regex(/^\d{5}$/, "Must 5 digit"),
@@ -12,7 +12,7 @@ export const addressSchema = z.object({
 
 export const updateAddressSchema = z.object({
   recipient: z.string().optional(),
-  label: z.enum(["rumah", "kantor", "kos", "sekolah", "apartemen"]).optional(),
+  label: z.enum(["Home", "Office", "Boarding House", "School", "Apartment"]).optional(),
   city: z.string().optional(),
   subdistrict: z.string().optional(),
   zip_code: z.string().regex(/^\d{5}$/, "Must 5 digit").optional(),
